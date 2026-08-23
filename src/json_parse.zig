292d00718f44189dc313e0739d44efaa8955cb9d
Paulos <paulos-git@protonmail.com>
2026-08-23 20:56:29 +0300

    add build.zig, improve README and some other improvements

--- src/json_parse.zig
+++ src/json_parse.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
 const main = @import("main.zig");
 
+/// Data we take from Openmeteo(or you can input in source other source, but doesn't tested)
 pub const data = struct {
     daily: struct {
-        temperature_2m_max: []f64,
-        temperature_2m_min: []f64,
-        wind_speed_10m_max: []f64,
-        precipitation_probability_max: []f64,
+        temperature_2m_max: ?[]f64,
+        temperature_2m_min: ?[]f64,
+        wind_speed_10m_max: ?[]f64,
+        precipitation_probability_max: ?[]f64,
     },
     current: struct {
         temperature_2m: f64,
@@ -19,23 +20,37 @@ pub const data = struct {
         precipitation: []u8,
     },
 };
+
 pub const stockConfig =
     \\ {"source":"https://api.open-meteo.com/v1/forecast?latitude=64.1355&longitude=-21.8954&daily=temperature_2m_max,temperature_2m_min,wind_speed_10m_max,precipitation_probability_max&current=temperature_2m,precipitation,wind_speed_10m&timezone=auto"}
 ;
 pub const rawConfig = struct {
     source: []const u8,
 };
+
 /// Read raw slice and transform to json
+///
 pub fn rawToJSON(comptime T: type, allocator: std.mem.Allocator, raw: []const u8) !std.json.Parsed(T) {
     const parsed = try std.json.parseFromSlice(T, allocator, raw, .{ .ignore_unknown_fields = true });
     return parsed;
 }
-/// TODO: add dynamically resolve $HOME(ssry for hardcode)
-///
+
 /// Check existence of dir and file, if doesn't exist - create with stockConfig
 pub fn configuration(allocator: std.mem.Allocator) !std.json.Parsed(rawConfig) {
-    const home = try std.process.getEnvVarOwned(allocator, "HOME");
-    const path = try std.fs.path.join(allocator, &[_][]const u8{ home, ".config/weazer" });
+    var path: []u8 = undefined;
+    if (std.process.getEnvVarOwned(allocator, "XDG_CONFIG_HOME")) |xdgDir| {
+        path = try std.fs.path.join(allocator, &[_][]const u8{ xdgDir, "weazer" });
+    } else |_| {
+        const home = try std.process.getEnvVarOwned(allocator, "HOME");
+        path = try std.fs.path.join(allocator, &[_][]const u8{ home, ".config/weazer" });
+    }
+
+    //{
+    //    var home = try std.process.getEnvVarOwned(allocator, "HOME");
+    //    const path = try std.fs.path.join(allocator, &[_][]const u8{ home, ".config/weazer" });
+    //} else {
+    //   const path = home;
+    //}
 
     std.fs.makeDirAbsolute(path) catch |err| switch (err) {
         error.PathAlreadyExists => {},
